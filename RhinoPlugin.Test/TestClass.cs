0dfdc587b5de6bb2671870ea62988591eff1a3b2
MingboPeng <mingo1214@gmail.com>
2020-02-16 12:17:19 -0500

    feat(Demo): add demo files

---
+++ RhinoPlugin.Test/TestClass.cs
@@ -0,0 +1,24 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rhino.DocObjects;
+
+namespace RhinoPlugin.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var doc = Rhino.RhinoDoc.ActiveDoc;
+            var guid = RhinoPluginCommand.Instance.MakeBrep(doc);
+            var rhinoObj = doc.Objects.FindId(guid);
+            var data = rhinoObj.Geometry.GetUserString("myData");
+
+            Assert.AreEqual(data, "myValue");
+        }
+    }
+}
---
+++ RhinoPlugin/RhinoPluginCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace RhinoPlugin
+{
+    public class RhinoPluginCommand : Command
+    {
+        public RhinoPluginCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static RhinoPluginCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "RhinoPluginCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            MakeBrep(doc);
+            doc.Views.Redraw();
+            return Result.Success;
+        }
+
+        public Guid MakeBrep(RhinoDoc doc)
+        {
+            var guid = Guid.Empty;
+            var bbox = new BoundingBox(new Point3d(0, 0, 0), new Point3d(5, 5, 5));
+            var brep = bbox.ToBrep();
+            brep.SetUserString("myData", "myValue");
+            guid = doc.Objects.AddBrep(brep);
+            return guid;
+        }
+    }
+
+
+
+}
